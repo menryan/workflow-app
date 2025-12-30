b59e08306f6c838b6eecb00fb91be89d6fe23fec
menryan <menryan@gmail.com>
2025-12-30 20:52:43 +0800

    updated NotifyTaskCompletedTest to check for non-completed task notification

--- tests/Feature/NotifyTaskCompletedTest.php
+++ tests/Feature/NotifyTaskCompletedTest.php
@@ -27,3 +27,23 @@ it('dispatches a notification job when task is completed', function () {
 
     Queue::assertPushed(NotifyTaskCompleted::class);
 });
+
+it('does not dispatches a notification job when task status is other than completed', function () {
+    Queue::fake();
+
+    $user = User::factory()->create();
+    $task = Task::factory()->create([
+        'status' => TaskStatus::DRAFT,
+        'project_id' => Project::factory()->create([
+            'owner_id' => $user->id,
+        ])->id,
+    ]);
+
+    app(UpdateTaskStatus::class)->execute(
+        $user,
+        $task,
+        TaskStatus::IN_PROGRESS
+    );
+
+    Queue::assertNotPushed(NotifyTaskCompleted::class);
+});
