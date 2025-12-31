f8f5c217f48dc5785dfe8581ce942dcb1553db89
menryan <menryan@gmail.com>
2025-12-31 18:08:30 +0800

    Reset Fortify back to original state with 2FA enabled and see users without 2fa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::factory()->create([
+        $user = User::factory()->withoutTwoFactor()->create([
             'email' => 'demo@example.com',
         ]);
 
