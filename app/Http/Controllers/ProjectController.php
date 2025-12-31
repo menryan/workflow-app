b3f31a764eb58c67b0458145c8199ddda6ca42c1
menryan <menryan@gmail.com>
2025-12-31 17:56:51 +0800

    Use Gate in controller for authorization

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ProjectController extends Controller
 {
     public function show(Project $project)
     {
-        $this->authorize('view', $project);
+        Gate::authorize('view', $project);
 
         return inertia('projects/show', [
             'project' => $project->load('tasks'),
